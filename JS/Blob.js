1f4dfe6cde426fd04793cf101080604703fb8c16
Jim <bottlewater072@gmail.com>
2017-11-24 00:36:52 -0500

    Continued tutorial for agar IO. Added functionality for eating other blobs

--- JS/Blob.js
+++ JS/Blob.js
@@ -1,13 +1,27 @@
-function Blob (x,y,r) {
-    this.pos = createVector(x,y);
-    this.r = r; 
-    
+function Blob(x, y, r) {
+    this.pos = createVector(x, y);
+    this.r = r;
+
     this.show = function () {
-        fill (255);
-        ellipse(this.pos.x,this.pos.y, this.r * 2, this.r * 2);
+        fill(255);
+        ellipse(this.pos.x, this.pos.y, this.r * 2, this.r * 2);
     }
-    
+
     this.update = function () {
-        var mouse = createVector()
+        var vel = createVector(mouseX - width / 2, mouseY - height / 2) //centers the blob relative to it's cetner
+        vel.setMag(2); //size
+        this.pos.add(vel);
+    }
+
+    this.eats = function (other) {
+        var d = p5.Vector.dist(this.pos, other.pos);
+        //checks if the distance between the two centers (big blob vs blob[i]) is smaller than their added radius{
+        if (d < this.r + other.r) {
+            this.r += other.r;
+            return true;
+        } else {
+            return false;
+        }
+
     }
 }
--- JS/Sketch.js
+++ JS/Sketch.js
@@ -3,17 +3,31 @@ var blobs = [];
 
 //creates blob ojbects
 function setup() {
-    createCanvas(600,600);
-    blob = new Blob(width/2,height/2,90);
-    for (var i = 0; i < 10; i++){
-        blobs [i] = new Blob (random(width),random(height),16);
+    createCanvas(600, 600);
+    blob = new Blob(width / 2, height / 2, 64);
+    for (var i = 0; i < 50; i++) {
+        var x = random(-width, width * 2);
+        var y = random(-width, width * 2);
+        blobs[i] = new Blob(random(x), random(y), 16);
     }
+
+    console.log('setup done')
 }
 
-function draw (){
-   background(0);
-    blob.show();
-   for (var i = 0; i < blobs.length; i++) {
+function draw() {
+    background(0);
+    translate(width / 2 - blob.pos.x, height / 2 - blob.pos.y);
+
+    
+    for (var i = blobs.length - 1; i >= 0; i--) {
         blobs[i].show();
-   }
+        
+        if (blob.eats(blobs[i])) {
+            blobs.splice(i, 1);
+        }
+    }
+    
+    blob.show();
+    blob.update();
+    console.log('draw function success');
 }
